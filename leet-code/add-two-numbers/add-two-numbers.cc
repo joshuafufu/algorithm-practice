fa0f9ee9fc4b365ba2f02a349fde0e454982b25b
joshuafufu <joshuafufu@gmail.com>
2013-02-20 22:47:22 -0800

    add two numbers.

--- leet-code/add-two-numbers/add-two-numbers.cc
+++ leet-code/add-two-numbers/add-two-numbers.cc
@@ -1,51 +1,73 @@
 #include "string.h"
 #include <iostream>
+#include <malloc.h>
 
 using namespace std;
 
 struct ListNode {
     int val;
     ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
 };
 
 class Solution {
 public:
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-        ListNode *current1;
-        ListNode *current2;
+        ListNode *head = new ListNode(0);
+        ListNode *cur = head;
+        int add = 0;
 
-        current1 = l1;
-        current2 = l2;
-
-        ListNode *result;
-
-        int step_num=0;
-        
-        int i=0, j=0, k=0;
-        while(current1!=NULL && current2 != NULL) {
-            result[i].val = (step_num+ current1->val + current2->val)%10;
-            step_num=(step_num+current2->val + current1->val)/10;
-            current1=current1->next;
-            current2=current2->next;
-            i++;
+        while(l1!=NULL || l2 !=NULL || add !=0) 
+        {
+            if (add!=0)
+            {
+                cur->val=add;
+                add =0;
+            }
             
-        }
+            if(l1!=NULL && l2==NULL)
+            {
+                add = (cur->val + l1->val)/10;
+                cur->val = (cur->val + l1->val)%10;
+            }
+            else if (l1 == NULL && l2!=NULL)
+            {
+                add = (cur->val + l2->val)/10;
+                cur->val = (cur->val+l2->val)%10;
+            }
+            if (l1==NULL && l2==NULL && add!=0)
+            {
+                cur->val = add;
+            }
+            if (l1!=NULL && l2!=NULL)
+            {
+                add = (cur->val + l1->val + l2->val)/10;
+                cur->val = (cur->val + l1->val + l2->val)%10;
+            }
 
-        while(current1!=NULL){
-            result[j].val= (step_num+current1->val)%10;
-            step_num=(step_num+current1->val)/10;
-            current1=current1->next;
-            j++;
-        }
+            if(l1==NULL || l1->next == NULL)
+            {
+                l1 = NULL;
+            }
+            else {
+                l1= l1->next;
+            }
 
-        while(current2!=NULL){
-            result[k].val= (step_num+current2->val)%10;
-            step_num=(step_num+current2->val)/10;
-            current2=current2->next;
-            k++;
-        }
+            if (l2==NULL || l2->next == NULL)
+            {
+                l2=NULL;
+            }else{
+                l2=l2->next;
+            }
+            if(l1!=NULL || l2!=NULL || add !=0)
+            {
+                cur->next = new ListNode(0);
+                cur=cur->next;
+            }
 
-        return result;
+        }
+        cur->next = NULL;
+        return head;
 
     }
 };
@@ -53,13 +75,17 @@ public:
 int main(int argc, char *argv[])
 {
     ListNode *a,*b;
-    a = new ListNode;
-    b = new ListNode;
-    a->val= 1;
-    b->val=2;
+    a = new ListNode(9);
+    b = new ListNode(9);
+    ListNode *a2 = new ListNode(9);
+    ListNode *b2 = new ListNode(9);
+    a->next=a2;
+    b->next=b2;
+   
     Solution sol;
     ListNode *c= sol.addTwoNumbers(a,b);
-    cout << c->val<<endl;
+    cout << c->next->val<<endl;
+    cout << c->next->next->val << endl;
     return 0;
 }
 
